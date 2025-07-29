2194814f522085a13d7a9f9a9bb32af6c9cd7850
rusho <anikhabib62@gmail.com>
2025-07-29 20:40:06 +0600

    Navbar added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,12 +1,15 @@
 import { Outlet } from "react-router";
 import Footer from "../Pages/Shares/Footer/Footer";
+import Navbar from "../Pages/Shares/Navbar/Navbar";
 
 const Main = () => {
   return (
     <>
-    <h1 className="">hello world</h1>
-    <Footer></Footer>
+    <div className="max-w-10/12 mx-auto">
+          <Navbar></Navbar>
       <Outlet />
+    <Footer></Footer>
+    </div>
     </>
   );
 };
