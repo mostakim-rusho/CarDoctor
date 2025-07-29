87ae7fa9d024ed772c3c6359e27f323cb3b3a2de
rusho <anikhabib62@gmail.com>
2025-07-30 02:55:25 +0600

    added

---
+++ src/Pages/Home/Services/Services.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+import ServicesCard from "./ServicesCard";
+
+const Services = () => {
+  const [services, setServices] = useState([]);
+  useEffect(() => {
+    fetch("services.json")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-8  ">
+      {services.map((service) => (
+        <ServicesCard key={service._id} service={service}></ServicesCard>
+      ))}
+    </div>
+  );
+};
+
+export default Services;
