e13fe885ad508124ef25cdd95c14c11eab498b3c
rusho <anikhabib62@gmail.com>
2025-08-06 00:54:26 +0600

    added

--- src/Pages/SignUp/Signup.jsx
+++ src/Pages/SignUp/Signup.jsx
@@ -1,11 +1,17 @@
-import { Link } from "react-router";
-
- 
+import { Link } from "react-router-dom";
 
 const Signup = () => {
-    const handleSignUp =event=>{
-        event.preventDefault
-    }
+  const handleSignUp = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    console.log("User Info:", { name, email, password });
+    // You can now send this data to your backend or Firebase
+  };
+
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
@@ -19,19 +25,22 @@ const Signup = () => {
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
           <div className="card-body">
-            <form onSubmit={handleSignUp} >
+            <form onSubmit={handleSignUp}>
               <fieldset className="fieldset">
                 <label className="label">Name</label>
-                <input type="text" name="name" className="input" placeholder="Name" />
+                <input type="text" name="name" className="input mb-2" placeholder="Name" />
+
                 <label className="label">Email</label>
-                <input type="email" name="email" className="input" placeholder="Email" />
+                <input type="email" name="email" className="input mb-2" placeholder="Email" />
+
                 <label className="label">Password</label>
                 <input
                   type="password"
-                   name="password" 
-                  className="input"
+                  name="password"
+                  className="input mb-2"
                   placeholder="Password"
                 />
+
                 <div>
                   <a className="link link-hover">Forgot password?</a>
                 </div>
@@ -41,9 +50,14 @@ const Signup = () => {
                   type="submit"
                   value="SignUp"
                 />
-              </fieldset> 
-            </form> 
-       <p>Already have and account <Link className="text-amber-700 font-bold" to='/login'>Login</Link></p>
+              </fieldset>
+            </form>
+            <p className="mt-4">
+              Already have an account?{" "}
+              <Link className="text-amber-700 font-bold" to="/login">
+                Login
+              </Link>
+            </p>
           </div>
         </div>
       </div>
