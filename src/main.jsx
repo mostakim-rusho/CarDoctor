381e59ded695ed26e17e665c3505e303f101904e
rusho <anikhabib62@gmail.com>
2025-08-05 23:37:41 +0600

    added

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import "./index.css";
 
 import {  RouterProvider } from "react-router-dom";
 import router from "./Routes/Routes";
+import AuthProvider from "./providers/AuthProvider";
 
  
 
@@ -12,6 +13,8 @@ import router from "./Routes/Routes";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+<AuthProvider>
+      <RouterProvider router={router} />
+</AuthProvider>
   </StrictMode>
 );
