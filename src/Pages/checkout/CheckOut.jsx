8e8f6aef2f192422969e01d62d87ed09a89f8ef5
rusho <anikhabib62@gmail.com>
2025-09-12 18:10:58 +0600

    checkout page

--- src/Pages/checkout/CheckOut.jsx
+++ src/Pages/checkout/CheckOut.jsx
@@ -1,11 +1,37 @@
- 
+import { useLoaderData } from "react-router-dom";
 
 const CheckOut = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const service = useLoaderData(); // ✅ must call as a function
+  const { title, _id } = service || {}; // ✅ safe destructure
+
+  return (
+    <div>
+      <h2>Checkout: {title}</h2>
+      <p>Service ID: {_id}</p>
+
+    
+        <form className=" mx-auto ">
+          <fieldset className="fieldset">
+            <label className="label">Email</label>
+            <input type="email" className="input" placeholder="Email" />
+            <label className="label">Password</label>
+            <input type="password" className="input" placeholder="Password" />
+          </fieldset>
+          <fieldset className="fieldset">
+            <label className="label">Email</label>
+            <input type="email" className="input" placeholder="Email" />
+            <label className="label">Password</label>
+            <input type="password" className="input" placeholder="Password" />
+          </fieldset>
+          <input
+            className="btn btn-active btn-primary"
+            type="submit"
+            value="order now"
+          />
+        </form>
+      </div>
+  
+  );
 };
 
 export default CheckOut;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,22 +4,31 @@ import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home/Home";
 import Login from "../Pages/Login/Login";
 import Signup from "../Pages/SignUp/Signup";
+import CheckOut from "../Pages/checkout/CheckOut";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
-    children: [{
-      path:'/',
-      element:<Home></Home>
-    },
-  {
-      path:'/login',
-      element:<Login></Login>
-    },
-   {
-      path:'/signup',
-      element: <Signup></Signup>
-    }],
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/signup",
+        element: <Signup></Signup>,
+      },
+      {
+        path: "/checkout/:id",
+        element: <CheckOut></CheckOut>,
+        loader: ({ params }) =>
+          fetch(`http://localhost:5000/services/${params.id}`),
+      },
+    ],
   },
 ]);
 export default router;
