889aedd780334b3c472c1a0348a3cf81886bd06a
rusho <anikhabib62@gmail.com>
2025-09-27 12:42:32 +0600

    added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -17,7 +17,7 @@ const router = createBrowserRouter([
       },
       {
         path: "/login",
-        element: <Login ></Login>,
+        element: <Login></Login>,
       },
       {
         path: "/signup",
